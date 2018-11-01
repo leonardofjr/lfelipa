a67e6218052daef8de0852660ace8e29d23b00e0
Leo <leonardo.f.jr@gmail.com>
2018-11-01 13:09:01 -0400

    updated

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,28 +1,26 @@
-<nav class="d-none d-sm-block">
-  <ul class="navbar fixed-top">
-    <div class="container">
-      <li class="nav-item"><a href="#top" class="nav-link">Home</a></li>
-      <li class="nav-item"><a href="#work" class="nav-link">Work</a></li>
-      <li class="nav-item"><a href="#skills" class="nav-link">Skills</a></li>
-      <li class="nav-item"><a href="#about" class="nav-link">About</a></li>
-      <li class="nav-item"><a href="#bottom" class="nav-link">Contact</a></li>
-    </div>
-  </ul>
-</nav>
 
-<nav class="d-block d-sm-none navbar navbar-dark bg-dark fixed-top p-3 mobile-navigation">
-  <button type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler">
+<nav class="navbar navbar-expand-lg container">
+  <a class="navbar-brand" href="#top">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="pos-f-t">
-    <div id="navbarToggleExternalContent" class="collapse bg-dark p-4">
-      <ul>
-      <li class="nav-item"><a href="#top" class="nav-link">Home</a></li>
-      <li class="nav-item"><a href="#Work" class="nav-link">Work</a></li>
-      <li class="nav-item"><a href="#skills" class="nav-link">Skills</a></li>
-      <li class="nav-item"><a href="#about" class="nav-link">About</a></li>
-      <li class="nav-item"><a href="#bottom" class="nav-link">Contact</a></li>
-      </ul>
-    </div>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#top">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#work">Work</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#skills ">Skills</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#bottom">Contact</a>
+      </li>
+    </ul>
   </div>
 </nav>
